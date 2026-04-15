a8852b5a303e69b37e00083fcea450bdd891f3ca
Mainor M <martimai000@warren.k12.in.us>
2026-04-15 13:57:56 -0400

    new init

--- core/src/main/java/io/github/found1/AnimationDemoScreen.java
+++ core/src/main/java/io/github/found1/AnimationDemoScreen.java
@@ -1,6 +1,7 @@
 package io.github.found1;
 
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.Screen;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
@@ -11,7 +12,10 @@ import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 public class AnimationDemoScreen implements Screen {
 
-    enum PlayerState {IDLE, RUNNING, JUMPING}
+    enum PlayerState {
+        IDLE,
+        RUNNING,
+        JUMPING}
 
     private SpriteBatch batch;
     private OrthographicCamera camera;
@@ -40,13 +44,68 @@ public class AnimationDemoScreen implements Screen {
         idleAnim = new Animation<>(0.2f, pGrid[0]);
         runAnim = new Animation<>(0.1f, pGrid[1]);
         jumpAnim = new Animation<>(0.15f, pGrid[2]);
+
+        idleAnim.setPlayMode(Animation.PlayMode.LOOP);
+        runAnim.setPlayMode(Animation.PlayMode.LOOP);
+        jumpAnim.setPlayMode(Animation.PlayMode.NORMAL);
+
+        enemySheet = new Texture("enemy-slime.png");
+        TextureRegion[][] eGrid = TextureRegion.split(enemySheet, 64, 64);
+        slimeAnim = new Animation<>(0.15f, eGrid[0]);
+        slimeAnim.setPlayMode(Animation.PlayMode.LOOP);
+
+        // TODO: coinSheet, cGrid, 0.08 for the frameDuration, and Loooooooooop
+        coinSheet = new Texture("coin.png");
+        TextureRegion[][] cGrid = TextureRegion.split(coinSheet, 32, 32);
+        coinAnim = new Animation<>(0.08f, cGrid[0]);
+        coinAnim.setPlayMode(Animation.PlayMode.LOOP);
     }
 
     @Override
     public void render(float delta) {
         stateTime += delta;
 
-        TextureRegion playerFrame = idleAnim.getKeyFrame(stateTime, true);
+        PlayerState previous = state;
+
+        if (Gdx.input.isKeyPressed(Input.Keys.SPACE)){
+            state = PlayerState.JUMPING;
+        }else if (Gdx.input.isKeyPressed(Input.Keys.LEFT)){
+            state = PlayerState.RUNNING;
+            facingRight = false;
+            playerX -= 100 * delta;
+        } else if (Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+            state = PlayerState.RUNNING;
+            facingRight = true;
+            playerX += 100 * delta;
+        }else {
+            state = PlayerState.IDLE;
+        }
+
+        if (previous != state){
+            stateTime = 0f;
+        }
+
+        Animation<TextureRegion> current;
+        switch(state){
+            case IDLE -> current = idleAnim;
+            case RUNNING -> current = runAnim;
+            case JUMPING -> current = jumpAnim;
+            default -> current = idleAnim;
+        }
+
+        boolean looping = state != PlayerState.JUMPING;
+        TextureRegion playerFrame = current.getKeyFrame(stateTime, looping);
+
+        if(!facingRight && !playerFrame.isFlipX()) {
+            playerFrame.flip(true, false);
+        } else if (facingRight && playerFrame.isFlipX()) {
+            playerFrame.flip(true, false);
+        }
+
+        TextureRegion slimeFrame = slimeAnim.getKeyFrame(stateTime, true);
+        TextureRegion coinFrame = coinAnim.getKeyFrame(stateTime, true);
+
+
         Gdx.gl.glClearColor(0.1f, 0.1f, 0.15f, 1f);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
@@ -55,11 +114,17 @@ public class AnimationDemoScreen implements Screen {
 
         batch.draw(playerFrame, playerX, playerY);
 
+        batch.draw(playerFrame, playerX, playerY);
+        batch.draw(slimeFrame, 400f, 100f);
+        batch.draw(coinFrame, 300f, 250f);
+        batch.draw(coinFrame, 340f, 250f);
+        batch.draw(coinFrame, 380f, 250f);
+
     }
 
     @Override
-    public void resize(int i, int i1) {
-
+    public void resize(int width, int height) {
+        camera.setToOrtho(false, width, height);
     }
 
     @Override
@@ -79,6 +144,10 @@ public class AnimationDemoScreen implements Screen {
 
     @Override
     public void dispose() {
+        batch.dispose();
+        playerSheet.dispose();
+        enemySheet.dispose();
+        coinSheet.dispose();
 
     }
 }
--- core/src/main/java/io/github/found1/Main.java
+++ core/src/main/java/io/github/found1/Main.java
@@ -1,32 +1,13 @@
 package io.github.found1;
 
-import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.Game;
 
-/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
-public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+public class Main extends Game {
 
     @Override
     public void create() {
-        setScreen(new AnimationDemoScreen())
+        setScreen(new AnimationDemoScreen());
 
-    }
-
-    @Override
-    public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
-    }
-
-    @Override
-    public void dispose() {
-        batch.dispose();
-        image.dispose();
     }
 }
+
